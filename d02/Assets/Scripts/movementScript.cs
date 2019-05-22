73415fd51319eb28362232a7c8fa3b185a611e68
Razshal <fontenille.mathieu@gmail.com>
2019-05-22 12:22:31 +0200

    ex00 - movement ok

---
+++ d02/Assets/Scripts/movementScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementScript : MonoBehaviour {
+    private Vector3 target;
+    private Vector3 rotation;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            rotation = target - transform.position;
+            rotation.Normalize();
+            transform.rotation = 
+                Quaternion.Euler(0f, 0f, Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg - 90);
+        }
+        if (Vector2.Distance(transform.position, target) > 0.01f)
+            transform.position = Vector2.MoveTowards(transform.position, target, 0.1f);
+	}
+}
